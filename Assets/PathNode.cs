bf515d963164188bce490e409d1e04b93e3b4561
stefankreitmayer <stefankreitmayer@gmail.com>
2017-07-22 14:31:40 +0200

    add path and traveller script

---
+++ Assets/PathNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PathNode : MonoBehaviour {
+	public PathNode[] branches;
+
+	void OnDrawGizmos() {
+		foreach (var branch in branches) {
+			Gizmos.color = Color.blue;
+			Gizmos.DrawLine(transform.position, branch.transform.position);
+		}
+	}
+
+	public PathNode PickBranch(){
+		return branches[Random.Range(0, branches.Length-1)];
+	}
+}
---
+++ Assets/Traveller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traveller : MonoBehaviour {	
+	public float speed;
+	public PathNode currentNode;
+
+	private PathNode nextNode;
+	private float phase = 0;
+
+	void Start () {
+		nextNode = currentNode.PickBranch();
+	}
+	
+	void Update () {
+		phase += speed * Time.deltaTime;
+		if (phase >= 1) {
+			currentNode = nextNode;
+			nextNode = currentNode.PickBranch();
+			phase = 0;
+		} else {
+			float curvedPhase = Mathf.Cos(phase * Mathf.PI) / -2f + .5f;
+			transform.position = Vector2.Lerp(currentNode.transform.position, nextNode.transform.position, curvedPhase);
+		}
+	}
+}
