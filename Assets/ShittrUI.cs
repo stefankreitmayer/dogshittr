6395e007e8f91f9e88aef9e27929e7f37a9acba7
Quinten Lansu <quintenlansu@gmail.com>
2017-07-22 14:15:26 +0200

    Improve Shittr UI.

---
+++ Assets/ShittrUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShittrUI : MonoBehaviour {
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+	}
+
+	public void onAcceptClicked()
+	{
+		Debug.Log("Accepted!");
+	}
+
+	public void onRejectClicked()
+	{
+		Debug.Log("Rejected!");
+	}
+
+}
