e92592c412de4fe2a10a9afdb277957c624d3cc2
s0554007 <s0554007@htw-berlin.de>
2017-07-22 14:05:50 +0200

    Drones and their sprites

---
+++ Assets/ClickableIncident.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableIncident : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
