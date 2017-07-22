4824b0181c94ff92d69ae80ee33b2994df4e413e
Quinten Lansu <quintenlansu@gmail.com>
2017-07-22 14:33:48 +0200

    Add random poop.

--- Assets/ShittrUI.cs
+++ Assets/ShittrUI.cs
@@ -5,17 +5,29 @@ using UnityEngine.UI;
 
 public class ShittrUI : MonoBehaviour {
 
+	public Sprite[] m_sprites;
+	public GameObject m_poopImage;
+
 	void Start()
 	{
+		PickPoop();
 	}
 
 	void Update()
 	{
 	}
 
+	public void PickPoop()
+	{
+		int index = Random.Range(0, m_sprites.Length);
+		m_poopImage.GetComponent<Image>().sprite = m_sprites[index];
+	}
+
 	public void onAcceptClicked()
 	{
 		Debug.Log("Accepted!");
+
+		PickPoop();
 	}
 
 	public void onRejectClicked()
