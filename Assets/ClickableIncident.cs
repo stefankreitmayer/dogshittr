5f4c83b219c7d0abe4c7132b080d8443e0875a55
Quinten Lansu <quintenlansu@gmail.com>
2017-07-22 15:50:00 +0200

    Display sprite based on user choice.

--- Assets/ClickableIncident.cs
+++ Assets/ClickableIncident.cs
@@ -5,29 +5,34 @@ using UnityEngine;
 public class ClickableIncident : MonoBehaviour {
 
 	public GameObject m_screen;
+	public Sprite m_spritePoop;
+	public Sprite m_spriteNotPoop;
+
 	private GameObject m_screenInstance;
+	private SpriteRenderer m_renderer;
+	private ShittrUI m_ui;
+	private bool m_isCorrect = false;
 
 	// Use this for initialization
 	void Start()
 	{
+		m_renderer = GetComponent<SpriteRenderer>();
 	}
 
 	void Destroy()
 	{
-		if (m_screenInstance != null)
-		{
-			var ui = m_screenInstance.GetComponent<ShittrUI>();
-			ui.OnAccepted -= OnUIAccepted;
-			ui.OnRejected -= OnUIRejected;
-		}
+		CloseScreen();
 	}
 
 	void OnMouseDown()
 	{
-		m_screenInstance = GameObject.Instantiate(m_screen);
-		var ui = m_screenInstance.GetComponent<ShittrUI>();
-		ui.OnAccepted += OnUIAccepted;
-		ui.OnRejected += OnUIRejected;
+		if (m_screenInstance == null)
+		{
+			m_screenInstance = GameObject.Instantiate(m_screen);
+			m_ui = m_screenInstance.GetComponent<ShittrUI>();
+			m_ui.OnAccepted += OnUIAccepted;
+			m_ui.OnRejected += OnUIRejected;
+		}
 	}
 
 	// Update is called once per frame
@@ -39,13 +44,34 @@ public class ClickableIncident : MonoBehaviour {
 	{
 		Debug.Log("Accepted!");
 
-		Destroy(m_screenInstance);
+		m_renderer.sprite = m_spritePoop;
+		m_isCorrect = m_ui.m_isPoop;
+
+		CloseScreen();
 	}
 
 	private void OnUIRejected(object sender, System.EventArgs e)
 	{
-		Debug.Log("Accepted!");
+		Debug.Log("Rejected!");
+
+		m_renderer.sprite = m_spriteNotPoop;
+		m_isCorrect = !m_ui.m_isPoop;
+
+		CloseScreen();
+	}
+
+	private void CloseScreen()
+	{
+		if (m_screenInstance == null)
+		{
+			return;
+		}
+
+		m_ui.OnAccepted -= OnUIAccepted;
+		m_ui.OnRejected -= OnUIRejected;
+		m_ui = null;
 
 		Destroy(m_screenInstance);
+		m_screenInstance = null;
 	}
 }
